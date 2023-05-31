5fe3f1a1fe3b35b6e673089aa31632142f195e60
Daniel Wagenius <wagenius.daniel@gmail.com>
2023-05-31 12:51:09 +0200

    first commit

---
+++ main.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include "uart.h"
+#include "max72xx.h"
+#include <string.h>
+#include <util/delay.h>
+
+#define BIT_SET(a, b) ((a) |= (1ULL << (b)))
+#define BIT_CLEAR(a,b) ((a) &= ~(1ULL<<(b)))
+#define BIT_FLIP(a,b) ((a) ^= (1ULL<<(b)))
+#define BIT_CHECK(a,b) (!!((a) & (1ULL<<(b)))) 
+
+#define MAX_COLUMNS 16
+#define MAX_ROWS 8
+
+#define JOYSTICK_BUTTON 2
+
+void clearScreen()
+{
+    for(int x = 0; x < MAX_COLUMNS; x++)
+    {
+        for(int y = 0; y < MAX_ROWS; y++)
+        {
+            max7219b_clr(x, y);
+            max7219b_out();
+        }
+    }
+}
+
+int main()
+{
+    BIT_CLEAR(DDRD, JOYSTICK_BUTTON);
+    BIT_SET(PIND, JOYSTICK_BUTTON); 
+
+    int x = 0;
+    int y = 0;
+
+	init_serial();
+	max7219_init();
+	while (1)
+    {
+        if(!BIT_CHECK(PIND, JOYSTICK_BUTTON))
+        {
+            clearScreen();
+        }
+
+        if(x == MAX_COLUMNS)
+        {
+            clearScreen();
+            x = 0;
+            y = 0;
+        }
+
+        if(y == MAX_ROWS)
+        {
+            x++;
+            y = 0;
+        }
+
+        max7219b_set(x, y);
+        max7219b_out();
+        _delay_ms(500);
+        y++;
+
+		// for(int x = 0; x < MAX_COLUMNS; x++)
+        // {
+		// 	for(int y = 0; y < MAX_ROWS; y++)
+        //     {
+                
+        //         max7219b_set(x, y);
+        //         max7219b_out();
+        //         _delay_ms(50);
+        //     }
+        // }
+	}
+	return 0;
+}
